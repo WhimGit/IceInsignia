6a91e1d4b015ec04b9c88d1fed5f4717f870bf24
DiggyDwarfHole <henry.porto001@mymdc.net>
2023-03-15 11:35:08 -0400

    New maps added

---
+++ Assets/Scripts/NotAttacking.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotAttacking : MonoBehaviour
+{
+    public void stopAttacking()
+    {
+        //WorldMovement.attacking = false;
+        GetComponent<Animator>().SetBool("attacking", false);
+    }
+}
